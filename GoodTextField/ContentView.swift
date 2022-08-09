bb062dfaae06cb8492d39b80ff726db09ff78cbd
Kame15-2021 <21cm0108@jec.ac.jp>
2022-08-09 17:21:21 +0900

    Initial Commit

---
+++ GoodTextField/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GoodTextField
+//
+//  Created by Ryo on 2022/08/09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GoodTextField/GoodTextFieldApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoodTextFieldApp.swift
+//  GoodTextField
+//
+//  Created by Ryo on 2022/08/09.
+//
+
+import SwiftUI
+
+@main
+struct GoodTextFieldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
